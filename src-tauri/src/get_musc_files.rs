9a2f053fe9530a7ab4bce3e5909973cb72cbec4f
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2024-02-27 19:38:53 +0900

    a

--- src-tauri/src/get_musc_files.rs
+++ src-tauri/src/get_musc_files.rs
@@ -1,69 +1,105 @@
-use std::{fs::{self, ReadDir}, path::PathBuf};
+use std::{
+    fs::{self, ReadDir},
+    path::PathBuf,
+};
 
 use crate::get_music_file;
+#[derive(Debug , Clone )]
 struct SongData {
-  music_file : String , 
-  music_name : String , 
-  music_dir : String
+    music_file: String,
+    music_name: String,
+    music_dir: String,
 }
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct MusicFile {
-  music_files: Vec<String>,
-  // music_datas : Vec<SongData>,
-  dir_path: String,
+    music_files: Vec<String>,
+    // music_datas : Vec<SongData>,
+    dir_path: String,
 }
-
 
 impl MusicFile {
-  fn new(dir_path: String) -> MusicFile {
-    MusicFile { dir_path , music_files: Vec::new()}
-  }
-  // bs のCustomLevelsのフォルダを取得する
-  fn get_music_dirs(&self) -> Vec<PathBuf> {
-    let mut file_list: Vec<PathBuf> = Vec::new();
-    let paths = fs::read_dir(&self.dir_path).unwrap();
-
-    for path in paths {
-      let path = path.unwrap().path();
-      file_list.push(path);
+    fn new(dir_path: String) -> MusicFile {
+        MusicFile {
+            dir_path,
+            music_files: Vec::new(),
+        }
     }
-    file_list
-  }
-  // bs のCustomLevelsのフォルダをから音楽ファイルを取得する
-  fn get_music_dirs_files(&self) -> Vec<String> {
-    let mut file_list: Vec<String> =  Vec::new();
-    let paths = self.get_music_dirs();
-    println!("paths : {:?}", paths);
-    for path in paths {
-      let files_paths= fs::read_dir(path).unwrap();
-      let music_file_list = self.filter_music_files(files_paths);
-      file_list.extend(music_file_list);
-    }
-    file_list
-  }
-  fn filter_music_files(&self ,file : ReadDir) -> Vec<String> {
-    let mut music_file_list: Vec<String> = Vec::new();
+    // bs のCustomLevelsのフォルダを取得する
+    fn get_music_dirs(&self) -> Vec<PathBuf> {
+        let mut file_list: Vec<PathBuf> = Vec::new();
+        let paths = fs::read_dir(&self.dir_path).unwrap();
 
-    for file in file {
-      let path = file.unwrap().path();
-      if path.extension().unwrap() == "wav" || path.extension().unwrap() == "mp3" || path.extension().unwrap() == "ogg" || path.extension().unwrap() == "flac" || path.extension().unwrap() == "aac" || path.extension().unwrap() == "m4a" || path.extension().unwrap() == "wma" || path.extension().unwrap() == "aiff" || path.extension().unwrap() == "alac" || path.extension().unwrap() == "dsd" || path.extension().unwrap() == "egg" {
-        music_file_list.push(path.to_str().unwrap().to_string());
-      }
-        
+        for path in paths {
+            let path = path.unwrap().path();
+            file_list.push(path);
+        }
+        file_list
     }
-    music_file_list
-  }
+    // bs のCustomLevelsのフォルダをから音楽ファイルを取得する
+    fn get_music_dirs_files(&self) -> Vec<String> {
+        let mut file_list: Vec<String> = Vec::new();
+        let paths = self.get_music_dirs();
+        println!("paths : {:?}", paths);
+        for path in paths {
+            let files_paths = fs::read_dir(path).unwrap();
+            let music_file_list = self.filter_music_files(files_paths);
+            file_list.extend(music_file_list);
+        }
+        println!("file_list : {:?}", file_list);
+        file_list
+    }
+    fn filter_music_files(&self, file: ReadDir) -> Vec<String> {
+        let mut music_file_list: Vec<String> = Vec::new();
 
+        for file in file {
+            let path = file.unwrap().path();
+            if path.extension().unwrap() == "wav"
+                || path.extension().unwrap() == "egg"
+                || path.extension().unwrap() == "mp3"
+                || path.extension().unwrap() == "ogg"
+                || path.extension().unwrap() == "flac"
+                || path.extension().unwrap() == "aac"
+                || path.extension().unwrap() == "m4a"
+                || path.extension().unwrap() == "wma"
+                || path.extension().unwrap() == "aiff"
+                || path.extension().unwrap() == "alac"
+                || path.extension().unwrap() == "dsd"
+            {
+                music_file_list.push(path.to_str().unwrap().to_string());
+            }
+        }
+        music_file_list
+    }
+
+    fn get_song_datas(&self) -> Vec<SongData> {
+        let mut file_list: Vec<SongData> = Vec::new();
+        let paths = self.get_music_dirs();
+        println!("paths : {:?}", paths);
+        for path in paths {
+            let files_paths = fs::read_dir(path.clone()).unwrap();
+            let music_file_list = self.filter_music_files(files_paths);
+            let song_data_temp = SongData {
+                music_file: music_file_list[0].clone(),
+                music_name: path.file_name().unwrap().to_string_lossy().to_string(),
+                music_dir: path.to_str().unwrap().to_string(),
+            };
+            file_list.push(song_data_temp);
+        }
+        println!("file_list : {:?}", file_list);
+        file_list
+    }
 }
-
 
 #[tauri::command]
 pub fn get_bs_music_files() -> Vec<String> {
-  let get_dir_path = "C:\\Users\\mochi\\BSManager\\SharedContent\\SharedMaps\\CustomLevels".to_string();
-  let file_list = MusicFile::new(get_dir_path);
-  file_list.get_music_dirs_files();
-  let mut bs_dir_list: Vec<PathBuf> = Vec::new();
-  let return_resutl = file_list.music_files;
-  println!("{:?}", return_resutl);
-  return_resutl
+    let get_dir_path =
+        "C:\\Users\\mochi\\BSManager\\SharedContent\\SharedMaps\\CustomLevels".to_string();
+    let file_list = MusicFile::new(get_dir_path);
+    file_list.get_music_dirs_files(ile_list.music_files
+
+    let mut bs_dir_list: Vec<PathBuf> = Vec::new();
+    let return_resutl = file_list.music_files;
+    println!("{:?}", return_resutl);
+    // println!("get song datas {:?}", file_list.get_song_datas());
+    return_resutl
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,7 +5,6 @@ use std::fs;
 mod get_musc_files;
 use get_musc_files::get_bs_music_files;
 
-
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -25,16 +24,27 @@ fn get_file_list(get_dir_path: String) -> Vec<String> {
 }
 // get_file_listから帰ってきたファイルに音楽ファイルがあったらそれを返す
 // なかったら何も返さない
-// wav, mp3, ogg, flac, aac, m4a, wma, aiff, alac, dsd , egg 
+// wav, mp3, ogg, flac, aac, m4a, wma, aiff, alac, dsd , egg
 // これらの拡張子を持つファイルがあったらそれを返す
 
 // それ以外のファイルがあったら何も返さない
 // なかったら何も返さない
 #[tauri::command]
-fn get_music_file(file_list: Vec<String> , base_dir_path : String) -> Vec<String> {
+fn get_music_file(file_list: Vec<String>, base_dir_path: String) -> Vec<String> {
     let mut music_file_list: Vec<String> = Vec::new();
     for file in file_list {
-        if file.ends_with(".wav") || file.ends_with(".mp3") || file.ends_with(".ogg") || file.ends_with(".flac") || file.ends_with(".aac") || file.ends_with(".m4a") || file.ends_with(".wma") || file.ends_with(".aiff") || file.ends_with(".alac") || file.ends_with(".dsd") || file.ends_with(".egg") {
+        if file.ends_with(".wav")
+            || file.ends_with(".mp3")
+            || file.ends_with(".ogg")
+            || file.ends_with(".flac")
+            || file.ends_with(".aac")
+            || file.ends_with(".m4a")
+            || file.ends_with(".wma")
+            || file.ends_with(".aiff")
+            || file.ends_with(".alac")
+            || file.ends_with(".dsd")
+            || file.ends_with(".egg")
+        {
             music_file_list.push(format!("{}/{}", base_dir_path, file))
         }
     }
@@ -43,7 +53,12 @@ fn get_music_file(file_list: Vec<String> , base_dir_path : String) -> Vec<String
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet, get_file_list,  get_music_file , get_bs_music_files]) 
+        .invoke_handler(tauri::generate_handler![
+            greet,
+            get_file_list,
+            get_music_file,
+            get_bs_music_files
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
