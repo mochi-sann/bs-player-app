78594610b8207ee274025f30e3f045e37180028e
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2024-02-27 19:06:49 +0900

    update

--- src-tauri/src/get_musc_files.rs
+++ src-tauri/src/get_musc_files.rs
@@ -1,14 +1,24 @@
-use std::{fs, path::PathBuf};
+use std::{fs::{self, ReadDir}, path::PathBuf};
 
 use crate::get_music_file;
+struct SongData {
+  music_file : String , 
+  music_name : String , 
+  music_dir : String
+}
+#[derive(Debug)]
 struct MusicFile {
+  music_files: Vec<String>,
+  // music_datas : Vec<SongData>,
   dir_path: String,
 }
 
+
 impl MusicFile {
   fn new(dir_path: String) -> MusicFile {
-    MusicFile { dir_path }
+    MusicFile { dir_path , music_files: Vec::new()}
   }
+  // bs のCustomLevelsのフォルダを取得する
   fn get_music_dirs(&self) -> Vec<PathBuf> {
     let mut file_list: Vec<PathBuf> = Vec::new();
     let paths = fs::read_dir(&self.dir_path).unwrap();
@@ -19,51 +29,41 @@ impl MusicFile {
     }
     file_list
   }
+  // bs のCustomLevelsのフォルダをから音楽ファイルを取得する
   fn get_music_dirs_files(&self) -> Vec<String> {
-    let mut file_list: Vec<String> = Vec::new();
-    let paths = fs::read_dir(&self.dir_path).unwrap();
+    let mut file_list: Vec<String> =  Vec::new();
+    let paths = self.get_music_dirs();
+    println!("paths : {:?}", paths);
     for path in paths {
-      let path = path.unwrap().path();
-      let file_name = path.file_name().unwrap().to_str().unwrap().to_string();
-      println!("{}", file_name);
-      file_list.push(file_name);
+      let files_paths= fs::read_dir(path).unwrap();
+      let music_file_list = self.filter_music_files(files_paths);
+      file_list.extend(music_file_list);
     }
     file_list
+  }
+  fn filter_music_files(&self ,file : ReadDir) -> Vec<String> {
+    let mut music_file_list: Vec<String> = Vec::new();
+
+    for file in file {
+      let path = file.unwrap().path();
+      if path.extension().unwrap() == "wav" || path.extension().unwrap() == "mp3" || path.extension().unwrap() == "ogg" || path.extension().unwrap() == "flac" || path.extension().unwrap() == "aac" || path.extension().unwrap() == "m4a" || path.extension().unwrap() == "wma" || path.extension().unwrap() == "aiff" || path.extension().unwrap() == "alac" || path.extension().unwrap() == "dsd" || path.extension().unwrap() == "egg" {
+        music_file_list.push(path.to_str().unwrap().to_string());
+      }
+        
+    }
+    music_file_list
   }
 
 }
 
+
 #[tauri::command]
 pub fn get_bs_music_files() -> Vec<String> {
-  let mut bs_dir_list: Vec<PathBuf> = Vec::new();
   let get_dir_path = "C:\\Users\\mochi\\BSManager\\SharedContent\\SharedMaps\\CustomLevels".to_string();
-  let paths = fs::read_dir(get_dir_path).unwrap();
-
-  for path in paths {
-    let path = path.unwrap().path();
-    let file_name = path.file_name().unwrap().to_str().unwrap().to_string();
-  
-    // ディレクトリかどうかを判定する
-    if !path.is_dir() {
-      continue;
-    }
-
-    println!("file list{}", path.canonicalize().unwrap().to_str().unwrap().to_string());
-    bs_dir_list.push(path);
-  }
-
-  println!("bs_dir_list{:?}", bs_dir_list);
-  let temp_files  : Vec<String> = Vec::new();
-  for bs in bs_dir_list {
-    // したいしたファイルにあったファイル一覧
-    let bs_files = fs::read_dir(bs).unwrap();
-    for file in bs_files {
-      let file = file.unwrap().path();
-      let file_name = file.file_name().unwrap().to_str().unwrap().to_string();
-
-    }
-  }
-  // let musics = get_music_file(bs, base_dir_path);
-
-  bs_dir_list
+  let file_list = MusicFile::new(get_dir_path);
+  file_list.get_music_dirs_files();
+  let mut bs_dir_list: Vec<PathBuf> = Vec::new();
+  let return_resutl = file_list.music_files;
+  println!("{:?}", return_resutl);
+  return_resutl
 }
