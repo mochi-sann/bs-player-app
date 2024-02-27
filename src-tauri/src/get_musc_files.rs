24c0ac5a8e7942d2c093f34da7219250ca96fd6e
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2024-02-27 20:22:40 +0900

    いい感じにsougDataを表示できるようになった

--- src-tauri/src/get_musc_files.rs
+++ src-tauri/src/get_musc_files.rs
@@ -3,9 +3,11 @@ use std::{
     path::PathBuf,
 };
 
+use serde::Serialize;
+
 use crate::get_music_file;
-#[derive(Debug , Clone )]
-struct SongData {
+#[derive(Debug , Clone , Serialize)]
+pub struct SongData {
     music_file: String,
     music_name: String,
     music_dir: String,
@@ -13,7 +15,7 @@ struct SongData {
 #[derive(Debug, Clone)]
 struct MusicFile {
     music_files: Vec<String>,
-    // music_datas : Vec<SongData>,
+    music_datas : Vec<SongData>,
     dir_path: String,
 }
 
@@ -22,6 +24,7 @@ impl MusicFile {
         MusicFile {
             dir_path,
             music_files: Vec::new(),
+            music_datas: Vec::new(),
         }
     }
     // bs のCustomLevelsのフォルダを取得する
@@ -91,15 +94,15 @@ impl MusicFile {
 }
 
 #[tauri::command]
-pub fn get_bs_music_files() -> Vec<String> {
+pub fn get_bs_music_files() -> Vec<SongData> {
     let get_dir_path =
         "C:\\Users\\mochi\\BSManager\\SharedContent\\SharedMaps\\CustomLevels".to_string();
     let file_list = MusicFile::new(get_dir_path);
-    file_list.get_music_dirs_files(ile_list.music_files
 
     let mut bs_dir_list: Vec<PathBuf> = Vec::new();
-    let return_resutl = file_list.music_files;
+    let return_resutl = &file_list.music_files;
+    let music_data = file_list.get_song_datas();
     println!("{:?}", return_resutl);
-    // println!("get song datas {:?}", file_list.get_song_datas());
-    return_resutl
+    println!("get song datas {:?}", music_data);
+    music_data
 }
