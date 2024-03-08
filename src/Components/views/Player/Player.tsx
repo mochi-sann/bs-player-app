972f9a8dd98ce8616c50e850c2b870772ccc8132
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2024-03-08 22:07:13 +0900

    音量バーを作成

--- src/Components/views/Player/Player.tsx
+++ src/Components/views/Player/Player.tsx
@@ -4,7 +4,7 @@ import { Center } from "@/Components/ui/center";
 import { Slider } from "@/Components/ui/slider";
 import { SongDataType } from "@/lib/types";
 import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
-import { Pause, Play, SkipBack, SkipForward } from "lucide-react";
+import { Pause, Play, SkipBack, SkipForward, Volume2 } from "lucide-react";
 import { createPortal } from "react-dom";
 
 type PlayerProps = {
@@ -31,28 +31,30 @@ export const Player = (props: PlayerProps) => {
   return (
     <div className="h-[97px]">
       {createPortal(
-        <div className="absolute bottom-0 left-0 flex w-full rounded-t-lg border bg-card/90 px-8 py-4 backdrop-blur">
+        <div className="absolute bottom-0 left-0 flex w-full gap-4 rounded-t-lg border bg-card/90 px-8 py-4 backdrop-blur">
           <PlayerContetWrapper>
-            {props.SongData && (
-              <Center className="flex h-full w-full flex-row gap-2">
-                <Avatar className="h-10 w-10 rounded">
-                  <AvatarImage
-                    src={props.SongData.image}
-                    alt="avatar"
-                    loading="lazy"
-                  />
-                  <AvatarFallback>
-                    <Fa6RegularImage height={32} width={32} />
-                  </AvatarFallback>
-                </Avatar>
-                <div className="align-center flex flex-col justify-center gap-0">
-                  <p className="text-xl  font-bold">
-                    {props.SongData.music_name}
-                  </p>
-                  <p className="text-sm">{props.SongData.auther}</p>
-                </div>
-              </Center>
-            )}
+            <div className="flex h-full justify-start">
+              {props.SongData && (
+                <Center className="flex h-full flex-row gap-2 ">
+                  <Avatar className="h-10 w-10 rounded">
+                    <AvatarImage
+                      src={props.SongData.image}
+                      alt="avatar"
+                      loading="lazy"
+                    />
+                    <AvatarFallback>
+                      <Fa6RegularImage height={32} width={32} />
+                    </AvatarFallback>
+                  </Avatar>
+                  <div className="align-center flex flex-col justify-center gap-0">
+                    <p className="text-xl  font-bold">
+                      {props.SongData.music_name}
+                    </p>
+                    <p className="text-sm">{props.SongData.auther}</p>
+                  </div>
+                </Center>
+              )}
+            </div>
           </PlayerContetWrapper>
           <PlayerContetWrapper>
             <div className=" flex max-w-[40rem] flex-col gap-4">
@@ -92,7 +94,20 @@ export const Player = (props: PlayerProps) => {
             </div>
           </PlayerContetWrapper>
           <PlayerContetWrapper>
-            <div></div>
+            <div className="flex h-full justify-end">
+              <Center className="flex h-full w-full max-w-48 gap-2 pl-6">
+                <Volume2 />
+                <Slider
+                  className=""
+                  max={props.max}
+                  step={0.01}
+                  value={[props.playingSec]}
+                  onMouseDown={handleMouseDown}
+                  onMouseUp={handleMouseUp}
+                  onValueChange={(e) => props.handleSeek(e[0])}
+                />
+              </Center>
+            </div>
           </PlayerContetWrapper>
         </div>,
         document.body,
